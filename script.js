0fe60d662a8b075319439ddbd6c4bd416b605191
jackkhoury06 <jackkhoury06@gmail.com>
2026-02-14 18:37:59 +0400

    add videos

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const themeSwitch = document.getElementById('themeSwitch');
 themeSwitch.addEventListener('change', () => {
   document.body.classList.toggle('light-theme');
 });
+
+// Image Modal
+const imageModal = document.getElementById('imageModal');
+if (imageModal) {
+  imageModal.addEventListener('show.bs.modal', function (event) {
+    const link = event.relatedTarget;
+    const imgSrc = link.getAttribute('data-img-src');
+    const modalImage = document.getElementById('modalImage');
+    modalImage.src = imgSrc;
+  });
+}
